a2fa81c582c09d9440165bbf0be14ed5d2ced8b7
Michael Aular <michaelaular@gmail.com>
2023-10-24 09:19:59 +0200

    update

--- resolvers.js
+++ resolvers.js
@@ -1,20 +1,20 @@
 import { GraphQLError } from "graphql";
 import { getCompany } from "./db/companies.js";
-import { createJob, getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+import { createJob, deleteJob, getJob, getJobs, getJobsByCompany, updateJob } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
     company: async (_root, { id }) => {
       const company = await getCompany(id);
       if (!company) {
-        throw notFoundError('No Company found with id '+ id);
+        throw notFoundError("No Company found with id " + id);
       }
       return company;
     },
     job: async (_root, { id }) => {
       const job = await getJob(id);
       if (!job) {
-        throw notFoundError('No Job found with id '+ id);
+        throw notFoundError("No Job found with id " + id);
       }
       return job;
     },
@@ -22,28 +22,42 @@ export const resolvers = {
   },
 
   Mutation: {
-    createJob: (_root, {input: {title, description}} ) => {
-      const companyId = 'FjcJCHJALA4i' // TODO set based on user
-      return createJob({ companyId, title, description })
+    createJob: (_root, { input: { title, description } }, { user }) => {
+      if (!user) {
+        throw unauthorizedError("Missing authentication");
+      }
+      return createJob({ companyId: user.companyId, title, description });
+    },
+
+    deleteJob: (_root, { id }) => deleteJob(id),
+
+    updateJob: (_root, { input: { id, title, description } }) => {
+      return updateJob({ id, title, description });
     },
   },
 
   Company: {
-    jobs: (company)=> getJobsByCompany(company.id),
+    jobs: (company) => getJobsByCompany(company.id),
   },
 
   Job: {
     company: (job) => getCompany(job.companyId),
-    date: (job) => toIsoDate(job.createdAt)
+    date: (job) => toIsoDate(job.createdAt),
   },
 };
 
 function notFoundError(message) {
   return new GraphQLError(message, {
-    extensions: { code: 'NOT_FOUND'}
+    extensions: { code: "NOT_FOUND" },
+  });
+}
+
+function unauthorizedError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: "UNAUTHORIZED" },
   });
 }
 
 function toIsoDate(value) {
-  return value.slice(0, 'yyyy-mm-dd'.length)
+  return value.slice(0, "yyyy-mm-dd".length);
 }
